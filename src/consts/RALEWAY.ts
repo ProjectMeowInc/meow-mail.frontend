b83f4fcf6549c8b4a61615208460a3378e8fce72
lx1cx <lagunov.2019@listy.ru>
2024-01-14 17:08:20 +0500

    Константа для шрифтов

---
+++ src/consts/RALEWAY.ts
@@ -0,0 +1,11 @@
+export const RALEWAY = {
+    Raleway_100Thin: "Raleway_100Thin",
+    Raleway_200ExtraLight: "Raleway_200ExtraLight",
+    Raleway_300Light: "Raleway_300Light",
+    Raleway_400Regular: "Raleway_400Regular",
+    Raleway_500Medium: "Raleway_500Medium",
+    Raleway_600SemiBold: "Raleway_600SemiBold",
+    Raleway_700Bold: "Raleway_700Bold",
+    Raleway_800ExtraBold: "Raleway_800ExtraBold",
+    Raleway_900Black: "Raleway_900Black"
+}
