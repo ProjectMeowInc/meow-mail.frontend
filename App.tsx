1715d5cfcbeb47944c17577b1a06f3d092300fcf
lx1cx <lagunov.2019@listy.ru>
2024-01-14 17:11:48 +0500

    Сделан вывод для авторизации

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Dimensions, SafeAreaView, StyleSheet, Text} from 'react-native';
+import AuthTab from "./app/(tabs)/AuthTab";
+import {COLORS} from "./src/consts/COLORS";
+import {
+  Raleway_200ExtraLight,
+  Raleway_100Thin,
+  Raleway_300Light,
+  Raleway_400Regular,
+  Raleway_500Medium,
+  Raleway_600SemiBold,
+  Raleway_700Bold,
+  Raleway_800ExtraBold,
+  Raleway_900Black,
+  useFonts
+} from "@expo-google-fonts/raleway";
+import React from "react";
 
 export default function App() {
+  const [fontLoaded, error] = useFonts({
+    Raleway_100Thin,
+    Raleway_200ExtraLight,
+    Raleway_300Light,
+    Raleway_400Regular,
+    Raleway_500Medium,
+    Raleway_600SemiBold,
+    Raleway_700Bold,
+    Raleway_800ExtraBold,
+    Raleway_900Black
+  })
+
+  if (!fontLoaded) {
+    return <Text>Loading...</Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <AuthTab/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+    backgroundColor: COLORS.black,
   },
 });
