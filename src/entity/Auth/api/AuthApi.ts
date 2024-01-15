872435626227f8a8b7dd61a9bc773cc4a9839576
Lx1Cx <lagunov.2019@listy.ru>
2024-01-16 00:55:20 +0500

    Написан клас деля взаимодействия api

---
+++ src/entity/Auth/api/AuthApi.ts
@@ -0,0 +1,36 @@
+import {Result} from "../../../shared/services/Result/Result";
+import {IAuthorizationRequest} from "../models/requests/IAuthorizationRequest";
+import {IAuthorizationResponse} from "../models/responses/IAuthorizationResponse";
+import {HTTPRequest} from "../../../shared/services/HTTPRequest/HTTPRequest";
+import {EmptyResult} from "../../../shared/services/EmptyError/EmptyResult";
+
+export class AuthApi {
+    
+    static async authorizationAsync(requestData: IAuthorizationRequest): Promise<Result<IAuthorizationResponse>> {
+        const result = await new HTTPRequest<IAuthorizationResponse>()
+            .withUrl("/v1/auth/authorization")
+            .withPOSTMethod()
+            .withBody(requestData)
+            .sendAsync()
+
+        if (result.hasError()) {
+            return Result.error(result.getError())
+        }
+
+        return Result.ok(result.unwrap())
+    }
+
+    static async registrationAsync(requestData: IAuthorizationRequest): Promise<EmptyResult> {
+        const result = await new HTTPRequest<void>()
+            .withUrl("/v1/auth/registration")
+            .withPOSTMethod()
+            .withBody(requestData)
+            .sendAsync()
+
+        if (result.hasError()) {
+            return EmptyResult.error(result.getError())
+        }
+
+        return EmptyResult.ok()
+    }
+}
