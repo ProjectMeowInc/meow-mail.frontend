df754f8ceff81e6c513cedb29f0f15e7ef054112
Lx1Cx <lagunov.2019@listy.ru>
2024-01-16 00:52:57 +0500

    Добавлен log level

--- src/consts/VARS.ts
+++ src/consts/VARS.ts
@@ -1,2 +1,5 @@
-const ApiServerIP = "SET UP"
+const ApiServerIP = "192.168.0.105:5000"
 export const ApiUrl = `http://${ApiServerIP}`
+
+type LogLevelType = "error" | "info"
+export const LOG_LEVEL: LogLevelType = "error"
