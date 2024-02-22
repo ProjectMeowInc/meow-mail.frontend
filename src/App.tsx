235e183fe147710666fefee1bebab1148f1732a2
Lx1Cx <lagunov.2019@list.ru>
2024-02-22 23:12:04 +0500

    Добавлена страница 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react"
 import { Route, Routes } from "react-router"
 import AuthPage from "./pages/auth/AuthPage/AuthPage"
-import RegistrationPage from "./pages/auth/RegistrationPage/RegistrationPage"
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
 
 function App() {
     return (
         <Routes>
             <Route path={"/"}>
                 <Route path={"login"} element={<AuthPage/>} />
-                <Route path={"registration"} element={<RegistrationPage/>} />
             </Route>
+            <Route path={"*"} element={<NotFoundPage/>} />
         </Routes>
     )
 }
