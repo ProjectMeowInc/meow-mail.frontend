9d16001772690916481efe7b0f320e6354825ce1
Lx1Cx <lagunov.2019@list.ru>
2024-02-22 21:45:17 +0500

    Написан сервис для уведомлений

---
+++ src/shared/services/AlertService.ts
@@ -0,0 +1,32 @@
+import { Bounce, toast } from "react-toastify"
+
+export class AlertService {
+
+    public static error(message: string) {
+        toast.error(message, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+    }
+
+    public static success(message: string) {
+        toast.success(message, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+    }
+}
