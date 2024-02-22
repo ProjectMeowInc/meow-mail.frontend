d11066c69e5142438b656d79f5e9bfab5576dc97
Lx1Cx <lagunov.2019@list.ru>
2024-02-22 21:43:12 +0500

    Доделана страница авторизации

--- src/pages/auth/AuthPage/AuthPage.tsx
+++ src/pages/auth/AuthPage/AuthPage.tsx
@@ -2,14 +2,18 @@ import React from "react"
 import Form from "../UI/Form/Form"
 import classes from "./authPage.module.css"
 import { Link } from "react-router-dom"
+import { useAuthPage } from "./useAuthPage"
 
 const AuthPage = () => {
+
+    const {ChangeHandler, SubmitHandler} = useAuthPage()
+
     return (
         <div className={classes.wrapper}>
             <h1 className={classes.logo}>MeowMail</h1>
             <div className={classes.modal}>
                 <p className={classes.caption}>Вход через логин и пароль</p>
-                <Form />
+                <Form onSubmit={SubmitHandler} onChange={ChangeHandler}/>
 
                 <div className={classes.links}>
                     <div className={classes.modal_item}>
