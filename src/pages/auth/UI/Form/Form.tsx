4353b72c4695d237d4ecf9679f4ecaa7b3ce6a19
Lx1Cx <lagunov.2019@list.ru>
2024-02-22 21:43:40 +0500

    Рефакторинг

--- src/pages/auth/UI/Form/Form.tsx
+++ src/pages/auth/UI/Form/Form.tsx
@@ -1,13 +1,19 @@
-import React from "react"
+import React, { FC, FormEvent } from "react"
 import Input from "../../../../shared/components/Input/Input"
 import Button from "../../../../shared/components/Button/Button"
 import classes from "./form.module.css"
+import { IOnChangeError, IOnChangeEvent } from "../../../../shared/events/IOnChangeEvent"
 
-const Form = () => {
+interface IFormProps {
+    onSubmit?: (e: FormEvent) => Promise<void>
+    onChange: (data: IOnChangeEvent) => IOnChangeError[] | void
+}
+
+const Form: FC<IFormProps> = ({onSubmit, onChange}) => {
     return (
-        <form className={classes.form} method={"post"}>
-            <Input type={"email"} name={"login"} placeholder={"Введите логин"}/>
-            <Input type={"email"} name={"password"} placeholder={"Введите пароль"}/>
+        <form className={classes.form} method={"post"} onSubmit={onSubmit}>
+            <Input onChange={onChange} type={"text"} name={"login"} placeholder={"Введите логин"}/>
+            <Input onChange={onChange} type={"password"} name={"password"} placeholder={"Введите пароль"}/>
             <Button>Отправить</Button>
         </form>
     )
