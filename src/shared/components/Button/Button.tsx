d5974ecbcd990b69e2f265e2ffd4c0bba7bf0ae9
Lx1Cx <lagunov.2019@list.ru>
2024-02-22 22:24:16 +0500

    Сделана блокировка кнопки при запросе

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -3,14 +3,24 @@ import classes from "./button.module.css"
 
 interface IButtonProps {
     onClick?: () => void
+    isLoading?: boolean
 }
 
-const Button: FC<PropsWithChildren<IButtonProps>> = ({children, onClick}) => {
+const Button: FC<PropsWithChildren<IButtonProps>> = ({children, onClick, isLoading}) => {
+
+    const ClickHandler = () => {
+        if (isLoading) {
+            return
+        }
+
+        onClick?.call(null)
+    }
+
     return (
         <button
             type={"submit"}
-            onClick={onClick}
-            className={classes.button}
+            onClick={() => ClickHandler()}
+            className={isLoading ? classes.button_loading : classes.button}
         >
             {children}
         </button>
