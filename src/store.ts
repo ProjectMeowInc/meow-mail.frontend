888bf470ca965d930d0b5b55da7c83b103fd0d43
Lx1Cx <lagunov.2019@list.ru>
2024-02-25 23:30:32 +0500

    Сделана синхронизации localstorage и rtk

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,46 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import { authApi } from "./entities/Auth/api/AuthApi"
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+import { userSlice } from "./entities/Auth/redusers/userSlice"
+import { persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+
+} from "redux-persist"
+import storage from "redux-persist/lib/storage" // defaults to localStorage for web
+
+const rootReducer = combineReducers({
+    [authApi.reducerPath]: authApi.reducer,
+    [userSlice.reducerPath]: userSlice.reducer
+})
+
+const persistConfig = {
+    key: "root",
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        [authApi.reducerPath]: authApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
+    reducer: persistedReducer,
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(authApi.middleware),
 })
+
+export const persistor = persistStore(store)
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
