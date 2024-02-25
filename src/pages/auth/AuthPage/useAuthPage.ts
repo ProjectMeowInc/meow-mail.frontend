e416c44bdbd2fff86f59ea2463f0e91021e82535
Lx1Cx <lagunov.2019@list.ru>
2024-02-25 23:32:24 +0500

    Сделана установка данных о пользователе в store

--- src/pages/auth/AuthPage/useAuthPage.ts
+++ src/pages/auth/AuthPage/useAuthPage.ts
@@ -4,10 +4,15 @@ import { IOnChangeEvent } from "../../../shared/events/IOnChangeEvent"
 import { AuthorizationDto } from "../../../entities/Auth/dto/AuthorizationDto"
 import { AlertService } from "../../../shared/services/AlertService"
 import { TokenService } from "../../../shared/services/TokenService"
+import { useAppDispatch } from "../../../store"
+import { useNavigate } from "react-router-dom"
+import { setUser } from "../../../entities/Auth/redusers/userSlice"
 
 export const useAuthPage = () => {
     const [authorization, { data, error, isLoading }] = useAuthorizationMutation()
     const [requestData, setRequestData] = useState<AuthorizationDto>({})
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (error && "data" in error) {
@@ -18,6 +23,12 @@ export const useAuthPage = () => {
             const { access_token, refresh_token } = data
             TokenService.setAccessToken(access_token)
             TokenService.setRefreshToken(refresh_token)
+            const {id, login} = TokenService.parseAccessToken(access_token)
+            dispatch(setUser({
+                id,
+                login
+            }))
+            navigate("/my")
         }
     }, [error, data, isLoading])
 
