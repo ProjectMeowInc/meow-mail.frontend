c0dcb3fac806b69013d9bd1f95ed5ab6379b52c4
Lx1Cx <lagunov.2019@list.ru>
2024-03-19 22:06:07 +0500

    Сделано закрытие меню по нажатию на любое пространство не связанное с меню

--- src/shared/components/Header/HeaderMobile/UI/useHeaderMobile.ts
+++ src/shared/components/Header/HeaderMobile/UI/useHeaderMobile.ts
@@ -1,14 +1,48 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useNavigate } from "react-router"
 
 export const useHeaderMobile = () => {
 
     const [isActive, setIsActive] = useState<boolean>(false)
     const navigate = useNavigate()
+    const menuRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        if (isActive) {
+            return
+        }
+
+        // Здесь логика закрытия меню по нажатию на любое пространство.
+        // Вешается click на document. После завершения обработчик удаляется
+
+        document.addEventListener("click", e => {
+            if (!menuRef.current) {
+                return
+            }
+
+            if (!menuRef.current?.contains(e.target as Node)) {
+                setIsActive(false)
+            }
+        })
+
+        return () => {
+            document.removeEventListener("click", e => {
+                if (!menuRef.current) {
+                    return
+                }
+
+                if (!menuRef.current?.contains(e.target as Node)) {
+                    setIsActive(false)
+                }
+            })
+        }
+
+    }, [isActive, setIsActive])
 
     return {
         isActive,
         setIsActive,
-        navigate
+        navigate,
+        menuRef
     }
 }
