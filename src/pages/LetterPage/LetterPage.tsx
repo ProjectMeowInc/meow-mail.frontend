cff2bdeef9235a4ec52ee023bb7a3533a479d3b9
Lx1Cx <lagunov.2019@list.ru>
2024-03-19 22:09:04 +0500

    Сделана страница для писем

--- src/pages/LetterPage/LetterPage.tsx
+++ src/pages/LetterPage/LetterPage.tsx
@@ -1,6 +1,10 @@
 import React from "react"
+import { useLettersPage } from "./useLettersPage"
 
 const LetterPage = () => {
+
+    const {mails, isLoading} = useLettersPage()
+
     return (
         <div>
 
---
+++ src/pages/LetterPage/useLettersPage.ts
@@ -0,0 +1,32 @@
+import { useSearchParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useGetAllEmailQuery } from "../../entities/Email/api/emailApi"
+import { AlertService } from "../../shared/services/AlertService"
+
+export const useLettersPage = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [pageNumber, setPageNumber] = useState<number>(Number(searchParams.get("page")) ?? 1)
+    const {data: mails, error, isLoading} = useGetAllEmailQuery(pageNumber)
+
+    useEffect(() => {
+
+        if (!searchParams.get("page")) {
+            return setPageNumber(1)
+        }
+
+        setPageNumber(Number(searchParams.get("page")))
+
+    }, [pageNumber])
+
+    useEffect(() => {
+        if (error && "data" in error) {
+            return AlertService.error(error.data.message)
+        }
+    }, [error])
+
+    return {
+        mails,
+        isLoading
+    }
+}
