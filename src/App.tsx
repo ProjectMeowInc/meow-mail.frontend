46d58e8c22352771bcd82f50dbe1f7cf215dea61
lx1cx <lagunov.2019@list.ru>
2024-03-19 13:30:35 +0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Route, Routes } from "react-router"
 import AuthPage from "./pages/auth/AuthPage/AuthPage"
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
 import RegistrationPage from "./pages/auth/RegistrationPage/RegistrationPage"
-import LettersPage from "./pages/my/LettersPage/LettersPage"
+import RootLayout from "./shared/layouts/RootLayout/RootLayout"
+import LetterPage from "./pages/LetterPage/LetterPage"
 
 function App() {
     return (
@@ -11,11 +12,9 @@ function App() {
             <Route path={"/"}>
                 <Route index element={<AuthPage />} />
                 <Route path={"registration"} element={<RegistrationPage />} />
-
-                <Route path={"my"}>
-                    <Route index element={<LettersPage/>}/>
+                <Route path={"my"} element={<RootLayout/>}>
+                    <Route index element={<LetterPage/>} />
                 </Route>
-
             </Route>
             <Route path={"*"} element={<NotFoundPage />} />
         </Routes>
