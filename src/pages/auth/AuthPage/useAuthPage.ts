e7cacd65c0e6e531d1962a2f12c2e1b7dd7933e7
Lx1Cx <lagunov.2019@list.ru>
2024-03-20 19:42:03 +0500

    Сделано создание ящика при авторизации

--- src/pages/auth/AuthPage/useAuthPage.ts
+++ src/pages/auth/AuthPage/useAuthPage.ts
@@ -1,5 +1,5 @@
 import { FormEvent, useEffect, useState } from "react"
-import { useAuthorizationMutation } from "../../../entities/Auth/api/AuthApi"
+import { useAuthorizationMutation, useCreateMailBoxMutation } from "../../../entities/Auth/api/AuthApi"
 import { IOnChangeEvent } from "../../../shared/events/IOnChangeEvent"
 import { AuthorizationDto } from "../../../entities/Auth/dto/AuthorizationDto"
 import { AlertService } from "../../../shared/services/AlertService"
@@ -13,24 +13,41 @@ export const useAuthPage = () => {
     const [requestData, setRequestData] = useState<AuthorizationDto>({})
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
+    const [createMailBox, {error: createMailBoxError}] = useCreateMailBoxMutation()
+
+    useEffect(() => {
+        if (data) {
+            const { access_token, refresh_token } = data
+
+            TokenService.setAccessToken(access_token)
+            TokenService.setRefreshToken(refresh_token)
+
+            const {id, login} = TokenService.parseAccessToken(access_token)
+
+            dispatch(setUser({
+                id,
+                login
+            }))
+
+            createMailBox().then()
+
+            navigate("/my?page=1")
+        }
+    }, [data, isLoading])
 
     useEffect(() => {
         if (error && "data" in error) {
             AlertService.error(error.data.message)
         }
+    }, [error])
 
-        if (data) {
-            const { access_token, refresh_token } = data
-            TokenService.setAccessToken(access_token)
-            TokenService.setRefreshToken(refresh_token)
-            const {id, login} = TokenService.parseAccessToken(access_token)
-            dispatch(setUser({
-                id,
-                login
-            }))
-            navigate("/my")
+    useEffect(() => {
+        if (createMailBoxError && "data" in createMailBoxError) {
+            if (createMailBoxError.data.error_type.split(".")[1] !== "MailBoxAlreadyExists") {
+                return AlertService.error(createMailBoxError.data.message)
+            }
         }
-    }, [error, data, isLoading])
+    }, [createMailBoxError])
 
     const ChangeHandler = ({ fieldName, fieldValue }: IOnChangeEvent) => {
         setRequestData((prevState) => ({
