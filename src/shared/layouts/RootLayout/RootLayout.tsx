675fed50c669dbba453f4e3871f4f44be656013a
Lx1Cx <lagunov.2019@list.ru>
2024-03-21 11:46:56 +0500

    Сделан каркас

--- src/shared/layouts/RootLayout/RootLayout.tsx
+++ src/shared/layouts/RootLayout/RootLayout.tsx
@@ -3,10 +3,11 @@ import { Outlet } from "react-router"
 import { useRootLayout } from "./useRootLayout"
 import HeaderMobile from "../../components/Header/HeaderMobile/HeaderMobile"
 import classes from "./rootLayout.module.css"
+import HeaderDesktop from "../../components/Header/HeaderDesktop/HeaderDesktop"
 
 const RootLayout = () => {
 
-    const {deviceType} = useRootLayout()
+    const {deviceType, user} = useRootLayout()
 
     return (
         <>
@@ -14,10 +15,26 @@ const RootLayout = () => {
                 ? <>
                     <HeaderMobile/>
                     <div className={classes.menu}/>
+                    <Outlet/>
                 </>
-                : <></>
+                : <div className={classes.wrapper}>
+                    <div className={classes.app}>
+                        <HeaderDesktop/>
+                        <div className={classes.outlet_wrapper}>
+                            <div className={classes.search}>
+                                <div className={classes.user}>
+                                    <div className={classes.user_image}>
+
+                                    </div>
+
+                                    <p>{user?.login}</p>
+                                </div>
+                            </div>
+                            <Outlet/>
+                        </div>
+                    </div>
+                </div>
             }
-            <Outlet/>
         </>
     )
 }
--- src/shared/layouts/RootLayout/useRootLayout.ts
+++ src/shared/layouts/RootLayout/useRootLayout.ts
@@ -1,9 +1,12 @@
 import { ClientService } from "../../services/ClientService"
+import { useAppSelector } from "../../../store"
 
 export const useRootLayout = () => {
     const deviceType = ClientService.getClientType()
+    const user = useAppSelector(state => state.user.data)
 
     return {
-        deviceType
+        deviceType,
+        user
     }
 }
