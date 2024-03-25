4e0be44f766f0f97179cef8f703ce195fc16a3de
Lx1Cx <lagunov.2019@list.ru>
2024-03-25 11:52:32 +0500

    Написан сервис для форматирования строк

---
+++ src/shared/services/FormatterService.ts
@@ -0,0 +1,18 @@
+/**
+ * Сервис для форматирования строк
+ */
+export class FormatterService {
+
+    /**
+     * Метод для форматирования даты
+     */
+    public static formatDate(time: number): string {
+
+        const formatter = new Intl.DateTimeFormat("ru", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+        })
+        return formatter.format(new Date(time))
+    }
+}
