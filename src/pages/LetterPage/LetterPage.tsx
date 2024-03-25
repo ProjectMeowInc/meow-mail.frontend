3f00fbe4ae7c0c35f06e83835f8568de9128de9d
Lx1Cx <lagunov.2019@list.ru>
2024-03-25 11:53:05 +0500

    Сделана страница письма

--- src/pages/LetterPage/LetterPage.tsx
+++ src/pages/LetterPage/LetterPage.tsx
@@ -1,29 +1,45 @@
 import React from "react"
-import { useLettersPage } from "./useLettersPage"
-import Email from "../../shared/components/Email/Email"
+import { useLetterPage } from "./useLetterPage"
+import DefaultUserImage from "../../shared/components/DefaultUserImage/DefaultUserImage"
 import classes from "./letterPage.module.css"
+import { FormatterService } from "../../shared/services/FormatterService"
+
+import {ReactComponent as Arrow} from "../../shared/icons/arrow-sm-up.svg"
+import {ReactComponent as Trash} from "../../shared/icons/trash.svg"
+import {ReactComponent as Star} from "../../shared/icons/star.svg"
+import { RedirectService } from "../../shared/services/RedirectService"
 
 const LetterPage = () => {
 
-    const {storeMails, isLoading} = useLettersPage()
+    const {mail, DeleteHandler} = useLetterPage()
 
-    // todo: change this
-    if (isLoading) {
+    // todo: fix this later
+    if (!mail) {
         return <>Loading...</>
     }
 
     return (
         <div className={classes.wrapper}>
-            {storeMails.map(mail => (
-                <Email
-                    key={mail.id}
-                    id={mail.id}
-                    href={`${mail.id}`}
-                    from={mail.from.mailbox}
-                    isRead={mail.is_read}
-                    subject={mail.subject}
-                />
-            ))}
+            <div className={classes.icons}>
+                <Arrow onClick={() => RedirectService.back()} className={classes.icon}/>
+
+                <div className={classes.right_side}>
+                    <Trash onClick={async () => DeleteHandler()} className={classes.icon}/>
+                    <Star className={classes.icon}/>
+                </div>
+            </div>
+            <h1 className={classes.email_subject}>{mail.subject}</h1>
+            <div className={classes.email_info}>
+                <DefaultUserImage/>
+                <div>
+                    <div className={classes.email_sender_info}>
+                        <p className={classes.email_sender}>{mail.from.mailbox}</p>
+                        <p className={classes.email_date}>{FormatterService.formatDate(mail.date_time * 1000)}</p>
+                    </div>
+                    <p className={classes.recipient_letter}>Кому: {mail.to.mailbox}</p>
+                </div>
+            </div>
+            <p className={classes.email_content}>{mail.content}</p>
         </div>
     )
 }
