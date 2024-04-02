c9a97ec763ac4aeac79f85af871ff19e8d40c238
lx1cx <lagunov.2019@list.ru>
2024-04-02 23:48:51 +0500

    Рефакторинг

--- src/pages/auth/UI/Form/Form.tsx
+++ src/pages/auth/UI/Form/Form.tsx
@@ -5,27 +5,31 @@ import classes from "./form.module.css"
 import { IOnChangeEvent } from "../../../../shared/events/IOnChangeEvent"
 import { useForm } from "./useForm"
 import { IInputError } from "../../../../shared/components/Input/IInputError"
+import Checkbox from "../../../../shared/components/Checkbox/Checkbox"
 
 interface IFormProps {
     onSubmit?: (e: FormEvent) => Promise<void>
     onChange: (data: IOnChangeEvent) => void
     isLoading: boolean
     inputErrors?: IInputError[]
+    isRegistration?: boolean
 }
 
-const Form: FC<IFormProps> = ({ onSubmit, onChange, isLoading, inputErrors }) => {
+const Form: FC<IFormProps> = ({ onSubmit, isRegistration, onChange, isLoading, inputErrors }) => {
     const { SubmitHandler } = useForm()
 
     return (
         <form className={classes.form} method={"post"} onSubmit={(e) => SubmitHandler(e, isLoading, onSubmit)}>
-            <Input error={inputErrors} onChange={onChange} type={"text"} name={"login"} placeholder={"Введите логин"} />
+            <Input required={true} error={inputErrors} onChange={onChange} type={"text"} name={"login"} placeholder={"Введите логин"} />
             <Input
+                required={true}
                 error={inputErrors}
                 onChange={onChange}
                 type={"password"}
                 name={"password"}
                 placeholder={"Введите пароль"}
             />
+            {isRegistration && <Checkbox label={"Я согласен(-на) на обработку персонельных данных"}/>}
             <Button isLoading={isLoading}>Отправить</Button>
         </form>
     )
