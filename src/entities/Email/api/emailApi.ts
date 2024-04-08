0c03c48b51a3812abdc7523133f972e5f3840bdd
lx1cx <lagunov.2019@list.ru>
2024-04-08 17:29:37 +0500

    Написана функция для сбора информации о запросе

--- src/entities/Email/api/emailApi.ts
+++ src/entities/Email/api/emailApi.ts
@@ -4,12 +4,12 @@ import { IGetAllEmailResponse } from "../models/responses/IGetAllEmailResponse"
 import { IGetAllEmailWIthFilterRequest } from "../models/requests/IGetAllEmailWIthFilterRequest"
 import { IGetAllEmailWithFilterResponse } from "../models/responses/IGetAllEmailWithFilterResponse"
 import { IUpdateEmailStatusRequest } from "../models/requests/IUpdateEmailStatusRequest"
-import { TokenService } from "../../../shared/services/TokenService"
 import { ISendEmailRequest } from "../models/requests/ISendEmailRequest"
 import { IGetEmailByIdResponse } from "../models/responses/IGetEmailByIdResponse"
 import { EmailGroup, EmailType } from "../../../consts"
 import { IGetEmailsByEmailGroupResponse } from "../../EmailGroup/models/responses/IGetEmailsByEmailGroupResponse"
 import { IGetEmailByEmailGroupRequest } from "../../EmailGroup/models/requests/IGetEmailByEmailGroupRequest"
+import { query } from "../../../shared/utils/query"
 
 export const emailApi = createApi({
     reducerPath: "emailApi",
@@ -19,13 +19,7 @@ export const emailApi = createApi({
     refetchOnFocus: true,
     endpoints: (build) => ({
         getAllEmail: build.query<IGetAllEmailResponse, number>({
-            query: (pageNumber) => ({
-                url: `/v1/email/my?page=${pageNumber}`,
-                method: "GET",
-                headers: {
-                    Authorization: TokenService.getAccessToken(),
-                },
-            }),
+            query: (pageNumber) => query(`/v1/email/my?page=${pageNumber}`, "GET", true),
             providesTags: (result) =>
                 result
                     ? [...result.items.map(({ id }) => ({ type: EmailType, id })), { type: EmailType, id: "LIST" }]
@@ -33,17 +27,10 @@ export const emailApi = createApi({
         }),
 
         getEmailWithFilter: build.query<IGetAllEmailWithFilterResponse, IGetAllEmailWIthFilterRequest>({
-            query: ({ pageNumber, subject, is_received }) => ({
-                url: "/v1/email/filter",
-                method: "GET",
-                headers: {
-                    Authorization: TokenService.getAccessToken(),
-                },
-                params: {
-                    page: pageNumber,
-                    subject,
-                    is_received,
-                },
+            query: ({ pageNumber, subject, is_received }) => query("/v1/email/filter", "GET", true, undefined, {
+                page: pageNumber,
+                subject,
+                is_received,
             }),
             providesTags: (result) =>
                 result
@@ -52,60 +39,28 @@ export const emailApi = createApi({
         }),
 
         getEmailsByEmailGroup: build.query<IGetEmailsByEmailGroupResponse, IGetEmailByEmailGroupRequest>({
-            query: ({page, email_group_id}) => ({
-                url: `/v1/email-group/find/${email_group_id}?page=${page}`,
-                method: "GET",
-                headers: {
-                    Authorization: TokenService.getAccessToken()
-                },
-            }),
+            query: ({page, email_group_id}) => query(`/v1/email-group/find/${email_group_id}?page=${page}`, "GET", true),
             providesTags: result => result
                 ? [...result.items.map(({ id }) => ({ type: EmailType, id })), { type: EmailType, id: "LIST" }]
                 : [{ type: EmailType, id: "LIST" }, { type: EmailGroup, id: "LIST" }],
         }),
 
         updateEmailStatus: build.mutation<void, IUpdateEmailStatusRequest>({
-            query: (body) => ({
-                url: "/v1/email/my/set-read",
-                method: "PUT",
-                body,
-                headers: {
-                    Authorization: TokenService.getAccessToken(),
-                },
-            }),
+            query: (body) => query("/v1/email/my/set-read", "PUT", true, body),
             invalidatesTags: [{ type: EmailType, id: "LIST" }, { type: EmailGroup, id: "LIST" }],
         }),
 
         sendEmail: build.mutation<void, ISendEmailRequest>({
-            query: (body) => ({
-                url: "/v1/email/send-message",
-                method: "POST",
-                body,
-                headers: {
-                    Authorization: TokenService.getAccessToken(),
-                },
-            }),
+            query: (body) => query("/v1/email/send-message", "POST", true, body),
             invalidatesTags: [{ type: EmailType, id: "LIST" }],
         }),
 
         getEmailById: build.query<IGetEmailByIdResponse, number>({
-            query: (emailId: number) => ({
-                url: `/v1/email/${emailId}`,
-                method: "GET",
-                headers: {
-                    Authorization: TokenService.getAccessToken(),
-                },
-            }),
+            query: (emailId: number) => query(`/v1/email/${emailId}`, "GET", true)
         }),
 
         deleteEmailById: build.mutation<void, number>({
-            query: (emailId: number) => ({
-                url: `/v1/email/${emailId}`,
-                method: "DELETE",
-                headers: {
-                    Authorization: TokenService.getAccessToken(),
-                },
-            }),
+            query: (emailId: number) => query(`/v1/email/${emailId}`, "DELETE", true),
             invalidatesTags: [{ type: EmailType, id: "LIST" }, { type: EmailGroup, id: "LIST" }],
         }),
     }),
