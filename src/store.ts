e9c7d5718f2fdab26818b3cb564abeee09235bec
lx1cx <lagunov.2019@list.ru>
2024-04-30 17:51:22 +0500

    Рудимент

--- src/store.ts
+++ src/store.ts
@@ -5,21 +5,17 @@ import { userSlice } from "./entities/Auth/redusers/userSlice"
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist"
 import storage from "redux-persist/lib/storage"
 import { emailApi } from "./entities/Email/api/emailApi"
-import { emailSlice } from "./entities/Email/slices/emailSlice"
 import { emailGroupApi } from "./entities/EmailGroup/api/EmailGroupApi"
 import { userApi } from "./entities/User/api/userApi"
 import { themeSlice } from "./entities/Theme/slices/themeSlice"
-import { pageSlice } from "./entities/Page/slices/pageSlice"
 
 export const rootReducer = combineReducers({
     [authApi.reducerPath]: authApi.reducer,
     [userSlice.reducerPath]: userSlice.reducer,
     [emailApi.reducerPath]: emailApi.reducer,
-    [emailSlice.reducerPath]: emailSlice.reducer,
     [emailGroupApi.reducerPath]: emailGroupApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
     [themeSlice.reducerPath]: themeSlice.reducer,
-    [pageSlice.reducerPath]: pageSlice.reducer,
 })
 
 const persistConfig = {
