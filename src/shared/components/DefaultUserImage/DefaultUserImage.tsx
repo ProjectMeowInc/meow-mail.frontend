cb2cbefa548bae53a42911e74b6980777c643e01
BlackSoulHub <mr.asaglaev@mail.ru>
2024-05-07 12:42:40 +0000

    fix force-push

--- src/shared/components/DefaultUserImage/DefaultUserImage.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import classes from "./defaultUserImage.module.css"
-
-const DefaultUserImage = () => {
-    return <div className={classes.user_image}></div>
-}
-
-export default DefaultUserImage
