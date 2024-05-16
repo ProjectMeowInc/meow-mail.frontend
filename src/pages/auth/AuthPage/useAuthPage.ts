a5ed1ca73ded327c8c99cd1bc32a8c0a07a03a43
lx1cx <lagunov.2019@list.ru>
2024-05-16 16:08:05 +0500

    fix bug

--- src/pages/auth/AuthPage/useAuthPage.ts
+++ src/pages/auth/AuthPage/useAuthPage.ts
@@ -14,7 +14,6 @@ import { setUser } from "../../../entities/Auth/redusers/userSlice"
 import { isCorrectError } from "../../../shared/utils/hasData"
 import { useFirstLoading } from "../../../shared/hooks/useFirstLoading"
 import { AuthService } from "../../../shared/services/AuthService"
-import { useEffectAsync } from "../../../shared/hooks/useEffectAsync"
 
 export const useAuthPage = () => {
     const [authorizationV2, { data, isLoading, error }] = useAuthorizationV2Mutation()
@@ -37,7 +36,58 @@ export const useAuthPage = () => {
         navigate("/my?page=1&is_received=true")
     })
 
-    useEffectAsync(async () => {
+    useEffect(() => {
+        if (isCorrectError(error)) {
+            AlertService.error(error.data.message)
+        }
+    }, [error])
+
+    // Здесь идёт проверка ошибки при создании ящика
+    // Если приходит отличная ошибка отличная от типа MailBoxAlreadyExists,
+    // то она будет выведена.
+    useEffect(() => {
+        if (isCorrectError(createMailBoxError)) {
+            if (createMailBoxError.data.error_type.split(".")[1] !== "MailBoxAlreadyExists") {
+                return AlertService.error(createMailBoxError.data.message)
+            }
+        }
+    }, [createMailBoxError])
+
+    const ChangeHandler = ({ fieldName, fieldValue }: IOnChangeEvent) => {
+        setRequestData((prevState) => ({
+            ...prevState,
+            [fieldName]: fieldValue,
+        }))
+    }
+
+    const SubmitHandler = async (e: FormEvent) => {
+        e.preventDefault()
+
+        if (!requestData) {
+            return AlertService.error("Поля не могут быть пустыми")
+        }
+
+        if (!requestData.login || !requestData.password) {
+            return AlertService.error("Поля не могут быть пустыми")
+        }
+
+        TokenService.removeRefreshToken()
+        TokenService.removeAccessToken()
+
+        cleanUpStore()
+
+        const result = await authorizationV2({
+            login: requestData.login,
+            password: requestData.password,
+            type: "Base",
+        })
+
+        if (!result.data) {
+            if (isCorrectError(result.error)) {
+                return AlertService.error(result.error.data.message)
+            }
+        }
+
         if (data && data.type === "Success") {
             const { access_token, refresh_token } = data
 
@@ -72,65 +122,70 @@ export const useAuthPage = () => {
             }
 
             navigate("/my?page=1&is_received=true")
-        }
-
-        if (data && data.type === "RequireTwoFactor") {
+        } else {
             setIsSuccess(true)
         }
-    }, [data])
-
-    useEffect(() => {
-        if (isCorrectError(error)) {
-            AlertService.error(error.data.message)
-        }
-    }, [error])
-
-    // Здесь идёт проверка ошибки при создании ящика
-    // Если приходит отличная ошибка отличная от типа MailBoxAlreadyExists,
-    // то она будет выведена.
-    useEffect(() => {
-        if (isCorrectError(createMailBoxError)) {
-            if (createMailBoxError.data.error_type.split(".")[1] !== "MailBoxAlreadyExists") {
-                return AlertService.error(createMailBoxError.data.message)
-            }
-        }
-    }, [createMailBoxError])
-
-    const ChangeHandler = ({ fieldName, fieldValue }: IOnChangeEvent) => {
-        setRequestData((prevState) => ({
-            ...prevState,
-            [fieldName]: fieldValue,
-        }))
     }
 
-    const SubmitHandler = async (e: FormEvent) => {
-        e.preventDefault()
-
-        if (!requestData) {
-            return AlertService.error("Поля не могут быть пустыми")
-        }
-
-        if (requestData.login && requestData.password) {
-            TokenService.removeRefreshToken()
-            TokenService.removeAccessToken()
-            cleanUpStore()
-            await authorizationV2({
-                login: requestData.login,
-                password: requestData.password,
-                type: "Base",
-            })
-        }
-    }
-
-    const SubmitCodeHandler = (event: FormEvent) => {
+    const SubmitCodeHandler = async (event: FormEvent) => {
         event.preventDefault()
 
-        if (data && data.type === "RequireTwoFactor") {
-            authorizationV2({
+        if (!data) {
+            return
+        }
+
+        if (data.type === "RequireTwoFactor") {
+            const result = await authorizationV2({
                 code: code,
                 request_id: data.request_id,
                 type: "TwoFactor",
             })
+
+            if (!result.data) {
+                if (isCorrectError(result.error)) {
+                    return AlertService.error(result.error.data.message)
+                }
+
+                return AlertService.error("Ошибка")
+            }
+
+            if (result.data.type === "Success") {
+                const { access_token, refresh_token } = result.data
+
+                TokenService.setAccessToken(access_token)
+                TokenService.setRefreshToken(refresh_token)
+
+                await createMailBox()
+
+                const getInformationResult = await getInformation()
+
+                if (isCorrectError(getInformationResult.error)) {
+                    return AlertService.error(getInformationResult.error.data.message)
+                }
+
+                if (!getInformationResult.data) {
+                    return AlertService.error("Ошибка")
+                }
+
+                const { id, login, role } = TokenService.parseAccessToken(TokenService.getAccessToken() ?? "")
+                const {
+                    user: { contains_two_factor, contains_mailbox },
+                    mailbox: { address },
+                } = getInformationResult.data
+
+                dispatch(
+                    setUser({
+                        id,
+                        login,
+                        role,
+                        contains_mailbox,
+                        contains_two_factor,
+                        mailbox: address,
+                    }),
+                )
+            }
+
+            navigate("/my?page=1&is_received=true")
         }
     }
 
